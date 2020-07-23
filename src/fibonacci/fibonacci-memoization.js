5c67907d1e6f839a8b5a4549e40950333f067b6a
dontkillthesparrows <post@althea.xyz>
2020-07-23 12:02:57 +0200

    correct fibonacci momoization with test

--- src/fibonacci/fibonacci-memoization.js
+++ src/fibonacci/fibonacci-memoization.js
@@ -2,7 +2,10 @@ function fibonacci(n, hash) {
   hash = hash || {};
 
   if (hash[n]) return hash[n];
-  if (n <= 1) return 1;
+  if (n === 0) return 0;
+  if (n === 1) return 1;
 
   return (hash[n] = fibonacci(n - 1, hash) + fibonacci(n - 2, hash));
 }
+
+module.exports = fibonacci;
