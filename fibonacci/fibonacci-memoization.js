4ab088bb202c85f0d6e27af1726a0750d748c487
dontkillthesparrows <post@althea.xyz>
2020-07-23 11:07:03 +0200

    removing files after movint to src

--- fibonacci/fibonacci-memoization.js
+++
@@ -1,8 +0,0 @@
-function fibonacci(n, hash) {
-  hash = hash || {};
-
-  if (hash[n]) return hash[n];
-  if (n <= 1) return 1;
-
-  return (hash[n] = fibonacci(n - 1, hash) + fibonacci(n - 2, hash));
-}
--- hash-table/hash.mjs
+++
@@ -1,8 +0,0 @@
-export function ASCIIhash(key, bucketsNumber) {
-  let hashCode = 0;
-  for (let charIndex = 0; charIndex < key.length; charIndex++) {
-    hashCode += key.charCodeAt(charIndex);
-  }
-
-  return hashCode % bucketsNumber;
-}
