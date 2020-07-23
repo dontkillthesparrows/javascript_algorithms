9db5df70fd8d7a1314a47f1fe5deae9820601d5f
dontkillthesparrows <post@althea.xyz>
2020-07-23 13:54:17 +0200

    corrected fibonacci with test

--- src/fibonacci/fibonacci-memoization.js
+++ src/fibonacci/fibonacci-memoization.js
@@ -2,8 +2,7 @@ function fibonacci(n, hash) {
   hash = hash || {};
 
   if (hash[n]) return hash[n];
-  if (n === 0) return 0;
-  if (n === 1) return 1;
+  if (n < 2) return n;
 
   return (hash[n] = fibonacci(n - 1, hash) + fibonacci(n - 2, hash));
 }
