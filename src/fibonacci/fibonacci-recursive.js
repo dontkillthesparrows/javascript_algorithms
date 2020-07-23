c53583fc973042fcb4630b6eec2f5a18b6eecd03
dontkillthesparrows <post@althea.xyz>
2020-07-23 12:00:18 +0200

    corrected recursive fibonacci with test

--- src/fibonacci/fibonacci-recursive.js
+++ src/fibonacci/fibonacci-recursive.js
@@ -1,5 +1,8 @@
 function fibonacci(num) {
-  if (num <= 1) return 1;
+  if (num === 0) return 0;
+  if (num === 1) return 1;
 
   return fibonacci(num - 1) + fibonacci(num - 2);
 }
+
+module.exports = fibonacci;
