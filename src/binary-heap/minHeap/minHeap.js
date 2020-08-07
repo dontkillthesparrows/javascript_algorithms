c3f8a76285cbfef86da569f08ba6e91a6e11eb73
dontkillthesparrows <post@althea.xyz>
2020-08-07 10:27:55 +0200

    minHeap with basic functionality

--- src/binary-heap/minHeap/minHeap.js
+++ src/binary-heap/minHeap/minHeap.js
@@ -7,12 +7,65 @@ class MinHeap {
     this.compare = new Comparator(comparatorFunction);
   }
 
+  getLeftChildIndex(index) {
+    return 2 * index + 1;
+  }
+
+  hasLeftChild(index) {
+    return this.getLeftChildIndex(index) < this.heapContainer.length;
+  }
+
+  getRightChildIndex(index) {
+    return 2 * index + 2;
+  }
+
+  hasRightChild(index) {
+    return this.getRightChildIndex(index) < this.heapContainer.length;
+  }
+
   getParentIndex(index) {
     return Math.floor((index - 1) / 2);
   }
 
   hasParent(index) {
     return this.getParentIndex(index) >= 0;
+  }
+
+  find(target, comparator = this.compare) {
+    const foundIndices = [];
+    let index = 0;
+    for (let item of this.heapContainer) {
+      if (comparator.equal(item, target)) {
+        foundIndices.push(index);
+      }
+      index++;
+    }
+    return foundIndices;
+  }
+
+  has(item) {
+    return this.heapMap.has(item);
+  }
+
+  peek() {
+    return this.heapContainer[0];
+  }
+
+  poll() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    if (this.heapContainer.length === 1) {
+      return this.heapContainer.pop();
+    }
+
+    const head = this.heapContainer[0];
+    this.heapContainer[0] = this.heapContainer.pop();
+
+    this.heapifyDown();
+
+    return head;
   }
 
   swap(index1, index2) {
@@ -34,6 +87,36 @@ class MinHeap {
     }
   }
 
+  heapifyDown(index = 0) {
+    let nextIndex = null;
+
+    while (this.hasLeftChild(index)) {
+      if (
+        this.hasRightChild(index) &&
+        this.compare.lessThanOrEqualTo(
+          this.heapContainer[this.getRightChildIndex(index)],
+          this.heapContainer[this.getLeftChildIndex(index)]
+        )
+      ) {
+        nextIndex = this.getRightChildIndex(index);
+      } else {
+        nextIndex = this.getLeftChildIndex(index);
+      }
+
+      if (
+        this.compare.lessThanOrEqualTo(
+          this.heapContainer[index],
+          this.heapContainer[nextIndex]
+        )
+      ) {
+        break;
+      } else {
+        this.swap(index, nextIndex);
+        index = nextIndex;
+      }
+    }
+  }
+
   add(item) {
     this.heapContainer.push(item);
     this.heapMap.set(item, item);
@@ -42,24 +125,34 @@ class MinHeap {
     return this;
   }
 
+  remove(item, comparator = this.compare) {
+    const indicesToRemove = this.find(item, comparator);
+
+    this.heapMap.delete(item);
+
+    for (let i of indicesToRemove) {
+      if (i === this.heapContainer.length - 1) {
+        this.heapContainer.pop();
+      } else {
+        this.heapContainer[i] = this.heapContainer.pop();
+
+        const parent = this.heapContainer[this.getParentIndex(i)];
+        if (this.hasLeftChild(i) && (!parent || parent <= item)) {
+          this.heapifyDown(i);
+        } else {
+          this.heapifyUp(i);
+        }
+      }
+    }
+    return this;
+  }
+
   isEmpty() {
     return !this.heapContainer.length > 0;
   }
 
-  peek() {
-    return this.heapContainer[0];
-  }
-
-  find(target) {
-    const foundIndices = [];
-    let index = 0;
-    for (let item of this.heapContainer) {
-      if (item === target) {
-        foundIndices.push(index);
-      }
-      index++;
-    }
-    return foundIndices;
+  toString() {
+    return this.heapContainer.toString();
   }
 }
 
