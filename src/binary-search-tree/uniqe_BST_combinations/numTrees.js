f0b90ea4bb3bb10f595ae27472df346673e5381d
dontkillthesparrows <post@althea.xyz>
2020-08-13 12:26:27 +0200

    explanation numTrees

--- src/binary-search-tree/uniqe_BST_combinations/numTrees.js
+++ src/binary-search-tree/uniqe_BST_combinations/numTrees.js
@@ -19,3 +19,22 @@ const numTrees = function (n) {
 };
 
 numTrees(5);
+// => 42
+
+/**
+ * F(i, n) = G(i-1)*G(n-i)
+ *
+ * G(n) represents the number of BST for a sequence of 1 to n
+ * F(i, n), 1 <= i <= n is the Cardesian product** of number of BST from its left and right sub-trees,
+ * where i is the root node and the range is from 1 to n.
+ *
+ * If F(3,7): where 2 is the root node, the total of node combinations is the Cartesian product of
+ * [1,2] and [4,5,6,7].
+ * Therefore F(3,7) = G(2) * G(4)
+ *
+ * G(n) = G(0) * G(n-1) + G(1) * G(n-2) + ... + G(n-1) * G(0)
+ *
+ *
+ * (**the Cardesian product is the multiplication of sets with ordered content.
+ * If A = {1,2} and B = {3,4} then the cardesian product is A*B = {1,3}, {2,4}, {1,4}, {2,3})
+ */
