f9bb861e9d6e605638d9669bbf8c6082c92dcc2f
dontkillthesparrows <post@althea.xyz>
2020-08-14 10:33:03 +0200

    sortByPriority with tests

---
+++ src/priority-queue/sortByPriority.js
@@ -0,0 +1,41 @@
+const sortByPriority = function (array) {
+  let container = new Array(array.length).fill(null);
+
+  for (let [value, priority] of array) {
+    let index = priority;
+
+    while (container[index] !== null && container[index][1] < priority) {
+      index++;
+    }
+
+    container.splice(index, 0, [value, priority]);
+    container.indexOf(null)
+      ? container.splice(container.indexOf(null), 1)
+      : undefined;
+  }
+
+  return container;
+};
+
+const sortDecendingThenPriority = function (array) {
+  array.sort((a, b) => {
+    if (b[0] === a[0]) {
+      return a[1] - b[1];
+    } else {
+      return b[0] - a[0];
+    }
+  });
+
+  let container = [];
+
+  for (let [value, priority] of array) {
+    container.splice(priority, 0, [value, priority]);
+  }
+
+  return container;
+};
+
+module.exports = {
+  sortByPriority,
+  sortDecendingThenPriority,
+};
---
+++ src/priority-queue/sortByPriority.test.js
@@ -0,0 +1,70 @@
+const {
+  sortByPriority,
+  sortDecendingThenPriority,
+} = require('./sortByPrioriy');
+
+test('should return sorted by priority', () => {
+  const array = [
+    [7, 0],
+    [4, 4],
+    [7, 1],
+    [5, 0],
+    [6, 1],
+    [5, 2],
+  ];
+
+  expect(sortByPriority(array)).toStrictEqual([
+    [5, 0],
+    [7, 0],
+    [6, 1],
+    [7, 1],
+    [5, 2],
+    [4, 4],
+  ]);
+});
+
+test('should sort decending by value, then increasing priority', () => {
+  const array = [
+    [7, 0],
+    [4, 4],
+    [7, 1],
+    [5, 0],
+    [6, 1],
+    [5, 2],
+  ];
+
+  expect(sortDecendingThenPriority(array)).toStrictEqual([
+    [5, 0],
+    [7, 0],
+    [5, 2],
+    [6, 1],
+    [4, 4],
+    [7, 1],
+  ]);
+
+  const array2 = [
+    [9, 0],
+    [7, 0],
+    [1, 9],
+    [3, 0],
+    [2, 7],
+    [5, 3],
+    [6, 0],
+    [3, 4],
+    [6, 2],
+    [5, 2],
+  ];
+
+  expect(sortDecendingThenPriority(array2)).toStrictEqual([
+    [3, 0],
+    [6, 0],
+    [7, 0],
+    [5, 2],
+    [3, 4],
+    [5, 3],
+    [6, 2],
+    [2, 7],
+    [9, 0],
+    [1, 9],
+  ]);
+});
