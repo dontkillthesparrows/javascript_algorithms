ad7c0fa559e4ce24ebfeb6573106300146a1d37a
dontkillthesparrows <post@althea.xyz>
2020-08-23 17:56:10 +0200

    faster sorting

--- src/priority-queue/orderlyQueue.js
+++ src/priority-queue/orderlyQueue.js
@@ -1,6 +1,10 @@
 var orderlyQueue = function (S, K) {
   if (K > 1) {
-    return S.split('').sort().join('');
+    return S.split('')
+      .sort((a, b) => {
+        return a.charCodeAt(0) - b.charCodeAt(0);
+      })
+      .join('');
   } else {
     let result = S;
     for (let i = 1; i < S.length; i++) {
